349f921c041e896ed7dc13d78aa960642188f29a
majingru <majingru@oneapm.com>
2020-03-23 18:07:29 +0800

    add barchar

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-import {BarChart} from './dchart'
+import {BarChart} from './chart'
 
 var bc = new BarChart({
     container: "#root", 
-    width : 200,
-    height: 200
+    width : 800,
+    height: 600
 });
 var dataset = [10,20,30,23,13,40,27,35,20];
 bc.data(dataset).render();
